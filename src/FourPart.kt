cd4e6a98161cc886795926ed4de2595c1e83174a
Federico Osella <osellafederico@gmail.com>
2018-08-22 12:07:26 +1200

    Function if number is prime or not

--- src/FourPart.kt
+++ src/FourPart.kt
@@ -5,6 +5,10 @@ fun main (args: Array<String>) {
 
     //Last step
     printerResultCalculators(calculatorFun, "sum",15,15)
+
+    //Check if number is prime
+    val numeroTest = 3
+    println("El numero $numeroTest es primo: ${isPrime(numeroTest)}")
 }
 
 //Do Functions! :)
@@ -25,4 +29,21 @@ fun calculatorTwoNumbersB(a: String, b: Int, c: Int) : String{
 //
 fun printerResultCalculators(function :(s: String, a: Int, b: Int) -> String,s:String, a: Int, b: Int){
     println(function(s,a,b))
+}
+
+
+//Function to know if a number is prime or not
+
+fun isPrime(numeroAnalyze: Int): Boolean{
+
+    var isPrime = true
+    for(index in 2..numeroAnalyze/2){
+        println("Index:"+index)
+        println("Index:"+numeroAnalyze + "///numeroAnalyze % index"+numeroAnalyze % index)
+        if(numeroAnalyze % index == 0){
+            isPrime = false
+            break
+        }
+    }
+    return isPrime
 }
